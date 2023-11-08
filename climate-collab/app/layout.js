f36a1e3112054b09dba0101ecaf5816c0c8e5457
Ljf227 <ljfrotton@gmail.com>
2023-11-08 16:32:37 -0500

    annoying db string pls work

--- climate-collab/app/layout.js
+++ climate-collab/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Climate Collab',
-  description: 'My Climate Collaborator',
+  description: 'Climate Collaborator',
 }
 
 export default function RootLayout({ children }) {
