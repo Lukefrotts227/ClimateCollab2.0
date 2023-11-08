306ae9fecff2f866059edb838e176985bd0cead7
Ljf227 <ljfrotton@gmail.com>
2023-11-07 22:05:36 -0500

    starting with the basics will go quicker than the community organizer will switch between the tow as they are both web apps

--- climate-collab/app/layout.js
+++ climate-collab/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen bg-gradient-to-tl from-green-400 to-blue-200`}>{children}</body>
     </html>
   )
 }
