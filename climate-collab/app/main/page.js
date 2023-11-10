fa75ae928d80ecaefb4ef1f971791ba55c9c87cb
Ljf227 <ljfrotton@gmail.com>
2023-11-10 14:14:46 -0500

    getting the layout of the main page now

--- climate-collab/app/main/page.js
+++ climate-collab/app/main/page.js
@@ -11,11 +11,25 @@ export default function Main(){
             <header className="h-12">
                 <SignOut />
             </header>
-            <section className = "grid place-items-center">  
-                <div className="h-12">
+            <section className = "grid grid-cols-4 grid-rows-7 gap-5">  
+                <div className="h-12 col-span-4 row-span-1">
                     <Greeting />
                 </div>
-                <BasicInfo />
+                <div className="col-span-2 row-span-2">
+                    <BasicInfo />
+                </div>
+                <div className="col-span-2 row-span-2">
+
+                </div>
+                <div className="col-span-2 row-span-2">
+
+                </div>
+                <div className="col-span-2 row-span-2">
+
+                </div>
+                <div className='col-span-4 row-span-2'>
+
+                </div>
             </section>
         </main>
     )
