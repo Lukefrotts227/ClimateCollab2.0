e7c2189c35af72a24b2018c05dffcec745eca4d4
Ljf227 <ljfrotton@gmail.com>
2023-11-10 20:51:18 -0500

    continue the form setup may switch to some backend work now

--- climate-collab/components/main/forms.js
+++ climate-collab/components/main/forms.js
@@ -2,28 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 const BasicInfo = () => {
     const [carChoice, setCarChoice] = useState(''); 
+    const [focusStore, setFocusStore] = useState([])
     const [milesPer, setMilesPer] = useState(-1);
+    
 
     return(
         <>
-            <div className="shadow-lg bg-blue-200 rounded-2xl p-8">
-                <form className='flex flex-col gap-3 items-center justify-center'>
-                    <div>
-                        <label>What kind of Car Do You Drive?</label>
-                        <select>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                            <option></option>
-                        </select>
-                    </div>                    
+            <div className="">
+                <div className="text-center p-3">
+                    <h1 className="font-bold text-2xl ">The important information</h1>
+                </div>
+                <div className="shadow-lg bg-blue-200 rounded-2xl p-8 border-2 border-blue-400">
+                    <form className='flex flex-col gap-3 items-center justify-center'>
+                        <div className="flex flex-col">
+                            <label>What kind of Car Do You Drive?</label>
+                            <select>
+                                <option></option>
+                                <option></option>
+                                <option></option>
+                                <option></option>
+                                <option>I don't drive</option>
+                            </select>
+                        </div>                    
 
-                    <div>
-                        <label></label>
-                        <input className="mx-6"/>
-                    </div>
-                </form>
+                        <div className="flex flex-col">
+                            <label>How many miles do you drive per week?</label>
+                            <input className="mx-6" type="number"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
@@ -33,18 +40,20 @@ const MainInfo = () => {
 
     return(
         <>
-            <div className="shadow-lg bg-blue-200 rounded-2xl p-8">
-                <form className='flex flex-col gap-3 items-center justify-center'>
-                    <div>
-                        <input className="mx-6"/>
-                    </div>                    
-                    <label></label>
-
-                    <div>
+            <div>
+                <div className="shadow-lg bg-blue-200 rounded-2xl p-8 border-2 border-blue-400">
+                    <form className='flex flex-col gap-3 items-center justify-center'>
+                        <div>
+                            <input className="mx-6"/>
+                        </div>                    
                         <label></label>
-                        <input className="mx-6"/>
-                    </div>
-                </form>
+
+                        <div>
+                            <label></label>
+                            <input className="mx-6"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
@@ -54,18 +63,20 @@ const SomeInfo = () => {
 
     return(
         <>
-            <div className="shadow-lg bg-blue-200 rounded-2xl p-8">
-                <form className='flex flex-col gap-3 items-center justify-center'>
-                    <div>
-                        <input className="mx-6"/>
-                    </div>                    
-                    <label></label>
-
-                    <div>
+            <div>
+                <div className="shadow-lg bg-blue-200 rounded-2xl p-8 border-2 border-blue-400">
+                    <form className='flex flex-col gap-3 items-center justify-center'>
+                        <div>
+                            <input className="mx-6"/>
+                        </div>                    
                         <label></label>
-                        <input className="mx-6"/>
-                    </div>
-                </form>
+
+                        <div>
+                            <label></label>
+                            <input className="mx-6"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
@@ -75,18 +86,20 @@ const ExtraInfo = () => {
 
     return(
         <>
-            <div className="shadow-lg bg-blue-200 rounded-2xl p-8">
-                <form className='flex flex-col gap-3 items-center justify-center'>
-                    <div>
-                        <input className="mx-6"/>
-                    </div>                    
-                    <label></label>
-
-                    <div>
+            <div>
+                <div className="shadow-lg bg-blue-200 rounded-2xl p-8 border-2 border-blue-400">
+                    <form className='flex flex-col gap-3 items-center justify-center'>
+                        <div>
+                            <input className="mx-6"/>
+                        </div>                    
                         <label></label>
-                        <input className="mx-6"/>
-                    </div>
-                </form>
+
+                        <div>
+                            <label></label>
+                            <input className="mx-6"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
