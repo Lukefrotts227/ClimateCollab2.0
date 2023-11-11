1f65f6ed8110e5d55ab050825214387e244d7202
Ljf227 <ljfrotton@gmail.com>
2023-11-11 17:17:43 -0500

    updated form, schema and added route .js file

--- climate-collab/components/main/forms.js
+++ climate-collab/components/main/forms.js
@@ -2,8 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 const BasicInfo = () => {
     const [carChoice, setCarChoice] = useState(''); 
-    const [focusStore, setFocusStore] = useState([false, false])
+    const [fuelChoice, setFuelChoice] = useState(''); 
+    const [focusStore, setFocusStore] = useState([false, false, false, false, false])
     const [milesPer, setMilesPer] = useState(-1);
+
+    const handleCarChoice = (e) =>{
+        e.preventDefault(); 
+        setCarChoice(e); 
+    }
+
+    const handleMilesPer = (e) =>{
+        e.preventDefault(); 
+        setMilesPer(e); 
+    }
+
+    const handleFuelChoice = (e) =>{
+        e.preventDefault(); 
+        setFuelChoice(e); 
+    }
 
     const handleFocus = (inp) =>{
         const updatedFocusStore = focusStore.map((item, i) => i === inp ? true : item);
@@ -28,21 +44,29 @@ const BasicInfo = () => {
                 <div className="shadow-lg bg-blue-200 rounded-2xl p-8 border-2 border-blue-400">
                     <form className='flex flex-col gap-3 items-center justify-center'>
                     {focusStore[0] && <p>the type of car driven can determine the carbon emissions</p>} 
-                    {focusStore[1] && <p>of course how much you drive matters!</p>}    
+                    {focusStore[1] && <p>The type of fuel you use in your car is important in finding out your emissions</p>}
+                    {focusStore[2] && <p>of course how much you drive matters!</p>}    
                         <div className="flex flex-col">
                             <label>What kind of Car Do You Drive?</label>
-                            <select onFocus={() => handleFocus(0)} onBlur={() => handleBlur(0)}>
+                            <select onChange={() => handleCarChoice(e)} onFocus={() => handleFocus(0)} onBlur={() => handleBlur(0)}>
                                 <option></option>
                                 <option></option>
                                 <option></option>
                                 <option></option>
                                 <option>I don't drive</option>
                             </select>
-                        </div>                    
+                        </div>        
+                        <div className="flex flex-col">
+                            <label>What kind of Fuel does it use</label>
+                            <select onChange={() => handleFuelChoice(e)} onFocus={() => handleFocus(1)} onBlur={() => handleBlur(0)}>
+                                <option></option>
+                                <option></option>
+                            </select>
+                        </div>            
 
                         <div className="flex flex-col">
                             <label>How many miles do you drive per week?</label>
-                            <input className="mx-6" type="number" onFocus={() => handleFocus(1)} onBlur= {() => handleBlur(1)} />
+                            <input className="mx-6" type="number" onFocus={() => handleFocus(2)} onBlur= {() => handleBlur(2)} />
                         </div>
                     </form>
                 </div>
