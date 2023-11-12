06ee038738cf8fcb44c427134f8bee0c2d39cec1
Ljf227 <ljfrotton@gmail.com>
2023-11-12 09:25:52 -0500

    api route for posting transport data

---
+++ climate-collab/app/api/user/postTransport/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { postTransBack } from "@/helpers/fetching/posting";
+
+async function handler(request){
+    const body = await request.json(); 
+    const content = body; 
+
+    if(!content || typeof content !== 'string'){
+        console.log("content not proper!!!"); 
+        return NextResponse.json({ error: 'Internal Server Error'}, { status: 500}); 
+    }
+
+    try{
+        const data = {fuel: content.fuel, car: content.car, miles: content.miles}
+        const userId = content.userID; 
+        const posting = await postTransBack(userId, data); 
+        return NextResponse.json({message: 'success'}, {status: 200})
+
+    }catch(error){
+        console.error(error); 
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500})   
+    }
+}
+
+export { handler as POST }; 
