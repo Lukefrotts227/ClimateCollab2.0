70a924cec5eb3af4b079dc0156c155b2b672fbfa
Ljf227 <ljfrotton@gmail.com>
2023-11-14 11:53:35 -0500

    finsihed vehicle calc not tested yet however

--- climate-collab/helpers/calculations/filter.js
+++ climate-collab/helpers/calculations/filter.js
@@ -1,9 +1,13 @@
 export const normalizeVehicle = (standard, base, max, newPerc) =>{
     const min = 0; 
     const inter = max - base;
+    const jumper = inter/100000;
     let percent; 
-
-
+    // for every variable jumper of emissions we add 1 percent. The difference is between standard and newPerc
+    // we need to add the difference to the base
+    let diff = newPerc - base;
+    let add = diff/jumper;
+    percent = standard + add;
 
 
     return percent; 
